af6fbc766590abcef89767ca9c12cedec0122425
Dandandan-mo <dan.mo6304@gmail.com>
2022-11-09 22:09:35 +1100

    modified errorhandlers.

--- main.py
+++ main.py
@@ -26,13 +26,10 @@ def create_app():
     def key_error(err):
         return {'error': f'The field {err} is required.'}, 400
 
-    @app.errorhandler(401)
-    def unauthorized(err):
-        return {'error': str(err)}, 401
-
     @app.errorhandler(404)
     def not_found(err):
         return {'error': str(err)}, 404
+        
     @app.errorhandler(400)
     def bad_request(err):
         return {'error': str(err)}, 400
