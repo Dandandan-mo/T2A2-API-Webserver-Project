a4d44e607dda566bbb718631e2f04414e1fb872b
Dandandan-mo <dan.mo6304@gmail.com>
2022-11-10 11:31:01 +1100

    hyprid property added.

--- models/order.py
+++ models/order.py
@@ -1,5 +1,6 @@
 from init import db, ma
 from marshmallow import fields
+from sqlalchemy.ext.hybrid import hybrid_property
 
 class Order(db.Model):
     __tablename__ = 'orders'
@@ -9,24 +10,21 @@ class Order(db.Model):
 
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
 
+    @hybrid_property
+    def total_payable(self):
+        total = 0
+        for order_product in self.order_products:
+            total += order_product.subtotal
+        return total
+
     user = db.relationship('User', back_populates='orders')
     order_products = db.relationship('OrderProduct', back_populates='order', cascade='all, delete')
-    # payment = db.relationship('Payment', back_populates='order', cascade= 'all, delete')
-    # shipment = db.relationship('Shipment', back_populates='order', cascade='all, delete')
 
 class OrderSchema(ma.Schema):
     user = fields.Nested('UserSchema', only=['first_name', 'last_name'])
     order_products = fields.List(fields.Nested('OrderProductSchema', exclude=['order_id']))
 
-    # total_payable = fields.Method("calc_total_payable")
-
-    # def calc_total_payable(self, order):
-    #     total = 0
-    #     for product in order.order_products:
-    #         total += product['payable']
-    #         return total
-
     class Meta:
-        fields = ('id', 'date', 'user_id', 'user', 'order_products')
+        fields = ('id', 'date', 'user_id', 'user', 'order_products', 'total_payable')
         ordered = True
 
--- models/order_product.py
+++ models/order_product.py
@@ -1,6 +1,7 @@
 from init import db, ma
 from marshmallow import fields
 from marshmallow.validate import Range
+from sqlalchemy.ext.hybrid import hybrid_property
 
 class OrderProduct(db.Model):
     __tablename__ = 'order_products'
@@ -11,16 +12,20 @@ class OrderProduct(db.Model):
     price = db.Column(db.Float, nullable=False)
     quantity = db.Column(db.Integer, nullable=False)
 
+    @hybrid_property
+    def subtotal(self):
+        return self.price * self.quantity
+
     order = db.relationship('Order', back_populates='order_products')
     product = db.relationship('Product', back_populates='order_products')
 
 class OrderProductSchema(ma.Schema):
     product = fields.Nested('ProductSchema', only=['name'])
-    payable = fields.Function(lambda order_product: order_product.price * order_product.quantity)
+    # payable = fields.Function(lambda order_product: order_product.price * order_product.quantity)
 
     product_id = fields.Integer(required=True, validate=Range(min=1, min_inclusive=True, error='Product id must be a positive integer.'))
     quantity = fields.Integer(required=True, validate=Range(min=1, min_inclusive=True, error='The mininum quantity is 1.'))
 
     class Meta:
-        fields = ('order_id', 'product_id', 'product', 'price', 'quantity', 'payable')
+        fields = ('order_id', 'product_id', 'product', 'price', 'quantity', 'subtotal')
         ordered = True
